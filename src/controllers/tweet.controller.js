c5c5b497c15e422bb4122d1aa220161b20ed0055
Aryan-1212 <aryanparvani23@gmail.com>
2025-04-01 16:51:43 +0530

    Add tweet management features: create, update, retrieve, and delete tweets with associated routes and model logic

---
+++ src/controllers/tweet.controller.js
@@ -0,0 +1,81 @@
+import { Tweet } from "../models/tweet.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import mongoose from "mongoose";
+
+
+const createTweet = (asyncHandler(async (req, res)=>{
+    const {content} = req.body
+
+    const tweet = await Tweet.create({
+        owner: req.user?._id,
+        content
+    })
+
+    if(!tweet) throw new ApiError(500, "Something went wrong while publishing tweet");
+
+    return res
+    .status(200)
+    .json(ApiResponse(200, tweet, "Tweet published successfully"))
+}))
+
+const updateTweet = (asyncHandler(async (req, res)=>{
+    const {tweetId} = req.params
+    const {content} = req.body
+
+    if(!tweetId?.trim()) throw new ApiError(400, "TweetId is missing");
+
+    const tweet = await Tweet.findByIdAndUpdate(new mongoose.Types.ObjectId(tweetId),{
+        content
+    },{
+        new: true,
+        runValidators: true
+    })
+
+    if(!tweet) throw new ApiError(500, "Something went wrong while updating tweet");
+
+    return res
+    .status(200)
+    .json(ApiResponse(200, tweet, "Tweet updated successfully"))
+}))
+
+const getUserTweets = (asyncHandler(async (req, res)=>{
+    const userId = req.user?._id
+
+    const tweets = await Tweet.aggregate([
+        {
+            $match: {
+                $owner: new mongoose.Types.ObjectId(userId)
+            }
+        },
+    ])
+
+    if(!tweets) throw new ApiError(404, "Tweets not found");
+
+    return res
+    .status(200)
+    .json(200, tweets, "Tweets fetched successfully")
+
+}))
+
+const deleteTweet = (asyncHandler(async (req, res)=>{
+    const {tweetId} = req.params
+
+    if(!tweetId?.trim()) throw new ApiError(400, "TweetId is missing");
+
+    const tweet = Tweet.findById(new mongoose.Types.ObjectId(tweetId))
+
+    if(!tweet) throw new ApiError(404, "Requested tweet not found");
+
+    const deleteVideo = await tweet.deleteOne()
+
+    if(!deleteTweet) throw new ApiError(500, "Something went wrong while deleting tweet");
+
+    return res
+    .status(200)
+    .json(200, deleteTweet, "Tweet deleted successfully")
+}))
+
+
+export {createTweet}
--- src/models/tweet.model.js
+++ src/models/tweet.model.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose'
 import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2'
+import { Like } from './like.model.js'
 
 const tweetSchema = new mongoose.Schema({
     owner:{
@@ -14,5 +15,11 @@ const tweetSchema = new mongoose.Schema({
 })
 
 tweetSchema.plugin(mongooseAggregatePaginate)
+
+tweetSchema.pre("deleteOne",{document: true, query: false}, async(next)=>{
+    const tweetId = this._id
+    await Like.deleteMany({tweet: tweetId})
+    next()
+})
 
 export const Tweet = mongoose.model("Tweet", tweetSchema)
